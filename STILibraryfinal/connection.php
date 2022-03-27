21fc583dbd42c826a4663f6b308984528c4d2aec
dfkjdfkj <102450282+dfkjdfkj@users.noreply.github.com>
2022-03-27 10:53:08 +0800

    Add files via upload

---
+++ STILibraryfinal/connection.php
@@ -0,0 +1,9 @@
+<?php // NAG CREATE NG CONNECTION FROM PHPMYADMIN SQL TO DITO
+$server="localhost";
+$username="root";
+$password="";
+$db="stilibraryfinal";
+
+$con= new mysqli ($server,$username,$password,$db);
+
+?>
